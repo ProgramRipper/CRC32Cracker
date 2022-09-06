863037db9e8d5736f0e02f2eaae8a9643a7d309a
ProgramRipper <programripper@foxmail.com>
2022-09-06 11:39:19 +0800

    refactor: enhance code quality

--- main.py
+++ main.py
@@ -1,74 +1,64 @@
-POLY = 0xEDB88320
+from itertools import count
+from typing import Literal
+
+POLY: Literal[0xEDB88320] = 0xEDB88320
+
+table: list[int] = []
+
+for i in range(256):
+    value = i
+    for _ in range(8):
+        if value & 1:
+            value = value >> 1 ^ POLY
+        else:
+            value >>= 1
+    table.append(value)
 
 
-class crc32Cracker:
-    def __init__(self):
-        self.table = self.table()
-
-    def crc32(self, string: str) -> (int, int):
-        INIT = 0xFFFFFFFF
-        index = None
-        for i in string:
-            index = (INIT ^ ord(i)) & 0xFF
-            INIT = INIT >> 8 ^ self.table[index]
-        return INIT, index
-
-    def lastIndex(self, crc: int) -> int:
-        for i in range(256):
-            if crc == self.table[i] >> 24:
-                return i
-
-    def check(self, high: int, indexes: list) -> (bool, str):
-        crc, index = self.crc32(str(high))
-        if index != indexes[3]:
-            return False
-        low = ''
-        for i in range(2, -1, -1):
-            num = crc & 0xFF ^ indexes[i]
-            if num not in range(48, 58):
-                return False
-            low += str(num - 48)
-            crc = self.table[indexes[i]] ^ crc >> 8
-        return low
-
-    @staticmethod
-    def table():
-        table = []
-        for i in range(256):
-            value = i
-            for _ in range(8):
-                if value & 1:
-                    value = value >> 1 ^ POLY
-                else:
-                    value >>= 1
-            table.append(value)
-        return table
-
-    def main(self, crc):
-        indexes = [0 for _ in range(4)]
-        crc = int(crc, 16) ^ 0xFFFFFFFF
-        for i in range(1, 1000):
-            if crc == self.crc32(str(i))[0]:
-                return i
-        for i in range(3, -1, -1):
-            index = indexes[3 - i] = self.lastIndex(crc >> (i << 3))
-            value = self.table[index]
-            crc ^= value >> ((3 - i) << 3)
-        i = 0
-        while True:
-            i += 1
-            low = self.check(i, indexes)
-            if low:
-                return int(str(i) + low)
-
-    def __call__(self, crc):
-        return self.main(crc)
+def crc32(string: str) -> tuple[int, int]:
+    crc: int = 0xFFFFFFFF
+    index: int = 0xFF
+    for i in string:
+        index = (crc ^ ord(i)) & 0xFF
+        crc = crc >> 8 ^ table[index]
+    return crc, index
 
 
-if __name__ == '__main__':
-    from time import time
+def last_index(crc: int) -> int:  # type: ignore
+    for index, value in enumerate(table):
+        if crc == value >> 24:
+            return index
+
+
+def check(high: int, indexes: list) -> int | None:
+    crc, index = crc32(str(high))
+    if index != indexes[3]:
+        return
+    high *= 1000
+    for i in range(2, -1, -1):
+        num = (crc & 0xFF ^ indexes[i]) - 48
+        if not 0 <= num < 10:
+            return
+        high += num * 10 ** i
+        crc = table[indexes[i]] ^ crc >> 8
+    return high
+
+
+def main(crc: str | int) -> int:  # type: ignore
+    indexes = [0] * 4
+    crc = (crc if isinstance(crc, int) else int(crc, 16)) ^ 0xFFFFFFFF
+    for i in range(1, 1000):
+        if crc == crc32(str(i))[0]:
+            return i
+    for i in range(3, -1, -1):
+        index = indexes[3 - i] = last_index(crc >> (i << 3))
+        crc ^= table[index] >> ((3 - i) << 3)
+    for i in count(1):
+        if result := check(i, indexes):
+            return result
+
+
+if __name__ == "__main__":
     from sys import argv
 
-    cracker = crc32Cracker()
-    t = time()
-    print(cracker(argv[1]), time() - t)
+    print(main(argv[1]))
